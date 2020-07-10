cd858aff9c33eff1448887ec4cb1fa34658c811a
KoboldM2 <mikoi.lukban@gmail.com>
2020-07-10 19:47:18 +0800

    Updates

---
+++ precache-manifest.e24aee654814f49c092fbd5736ff4f0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f51816c539274435af5d9b66cdbb3db",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "f276d93b42b5df04ec6d",
+    "url": "/Portfolio/static/js/2.396aaa39.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.396aaa39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d05a468a725065aeadd5",
+    "url": "/Portfolio/static/js/main.bd4cc2de.chunk.js"
+  },
+  {
+    "revision": "bbe3f87424f235aab386",
+    "url": "/Portfolio/static/js/runtime-main.8f55ad2e.js"
+  }
+]);
